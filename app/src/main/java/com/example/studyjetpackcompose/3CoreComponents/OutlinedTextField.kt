567922c4e2245129808d1bfd6b27849a06e5949e
wang <91462854+JiaLiDunK@users.noreply.github.com>
2025-08-08 17:24:03 +0800

    核心概念1

---
+++ app/src/main/java/com/example/studyjetpackcompose/3CoreComponents/OutlinedTextField.kt
@@ -0,0 +1,23 @@
+package com.example.studyjetpackcompose.`3CoreComponents`
+
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+
+@Composable
+fun OutlinedTextField() {
+    var text by remember {
+        mutableStateOf("")
+    }
+    OutlinedTextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text("Label") }
+    )
+}
+
