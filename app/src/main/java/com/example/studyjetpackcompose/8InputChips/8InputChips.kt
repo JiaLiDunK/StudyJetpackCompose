0d2d825c66dd7132682a4a73f649f5e972aae7a9
king <3371715457@qq.com>
2025-08-24 15:24:18 +0800

    输入完

--- app/src/main/java/com/example/studyjetpackcompose/8InputChips/8InputChips.kt
+++ app/src/main/java/com/example/studyjetpackcompose/8InputChips/8InputChips.kt
@@ -1,11 +1,60 @@
 package com.example.studyjetpackcompose.`8InputChips`
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.InputChip
+import androidx.compose.material3.InputChipDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.LineHeightStyle
+
 import androidx.compose.ui.tooling.preview.Preview
 
 
 @Composable
 fun InputChipsSample() {
+    var enable by remember(){
+        mutableStateOf(true)
+    }
+    Box(
+        modifier =  Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ){
+//        Text("看这块")
+        if (enable){
+            InputChip(
+                onClick = {
+                    enable = !enable
+                },
+                label = {
+                    Text("启动")
+                },
+                selected = enable,
+                avatar = {
+                    Icon(Icons.Filled.Person,
+                        "加载的表述",
+                        Modifier.size(InputChipDefaults.AvatarSize))
+                },
+                trailingIcon = {
+                    Icon(Icons.Filled.Close,
+                        "加载的表述",
+                        Modifier.size(InputChipDefaults.AvatarSize))
+                }
+            )
+        }
+    }
 
 }
 
---
+++ app/src/main/java/com/example/studyjetpackcompose/9AlertDialogBox/AlertDialogBox.kt
@@ -0,0 +1,17 @@
+package com.example.studyjetpackcompose.`9AlertDialogBox`
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun AlertDialogBoxSample() {
+
+
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun AlertDialogBoxSamplePreview() {
+    AlertDialogBoxSample()
+}
