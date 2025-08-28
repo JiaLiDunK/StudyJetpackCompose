077fa94920b721b87b1f1704d5d82c676cf64900
wang <91462854+JiaLiDunK@users.noreply.github.com>
2025-08-28 10:56:59 +0800

    搜索栏-开始

---
+++ app/src/main/java/com/example/studyjetpackcompose/16SearchBar/SearchBar.kt
@@ -0,0 +1,16 @@
+package com.example.studyjetpackcompose.`16SearchBar`
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun SearchBar() {
+
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun SearchBarPreview() {
+
+}
